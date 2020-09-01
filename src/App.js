3b91eee3f6ef774a96c83c385055d6c392118481
Gus Robinson <gus.m.robinson@gmail.com>
2020-09-01 18:41:56 -0400

    changes for local dev server troubleshoot

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+import { withAuthenticator , AmplifySignOut } from '@aws-amplify/ui-react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>
-            <span>T&uacute;</span>ber
-          </h1>
-          <h2>
-            Welcome to T&uacute;ber!
-          </h2>
-        </header>
-        <footer>
-          &copy; Gus Robinson 2020
-        </footer>
-      </div>
-    );
-  }
+function App () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          <span className='tuber-color'>T&uacute;</span>ber
+        </h1>
+        <h2>
+          Welcome to T&uacute;ber!
+        </h2>
+      </header>
+      <AmplifySignOut />
+      <footer>
+        &copy; Gus Robinson 2020
+      </footer>
+    </div>
+  );
 }
 
-export default withAuthenticator(App, true);
+export default withAuthenticator(App);
